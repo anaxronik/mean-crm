e19ecfd650d54739c7f7e00072d2944091bbd443
Sergey Komarov <anaxronik@gmail.com>
2020-12-01 00:11:38 +0500

    create auth routes

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const authRoute = require('./routes/auth')
 
 const app = express()
+
+app.use('/api/auth', authRoute)
 
 module.exports = app
---
+++ controllers/auth.js
@@ -0,0 +1,7 @@
+module.exports.login = (req, res) => {
+  res.status(200).json({ message: 'login' })
+}
+
+module.exports.register = (req, res) => {
+  res.status(200).json({ message: 'register' })
+}
