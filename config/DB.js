c14a71c97200a552b8b25f01d4fa427ab27c8695
Sergey Komarov <anaxronik@gmail.com>
2020-12-01 02:25:53 +0500

    create user in db

--- config/DB.js
+++ config/DB.js
@@ -1,1 +1,2 @@
-module.exports.MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017'
+module.exports.MONGO_URI =
+  process.env.MONGO_URI || 'mongodb://localhost:27017/mern-crm'
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/User')
+
 module.exports.login = (req, res) => {
   res.status(200).json({
     message: 'ok',
@@ -8,6 +10,20 @@ module.exports.login = (req, res) => {
 }
 
 module.exports.register = (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  const user = new User({ email, password })
+  user
+    .save()
+    .then(() => {
+      console.log('User has been created')
+    })
+    .catch((err) => {
+      if (err.code === 11000) {
+        console.log('email allready in use')
+      }
+      console.log(err.index)
+    })
+
   res.status(200).json({ message: 'ok' })
-  console.log('register')
 }
