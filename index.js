e61d1ba2911750586c16c70241fe1366167cd3e6
Sergey Komarov <anaxronik@gmail.com>
2020-11-30 23:54:16 +0500

    add start scripts

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-const express = require('express')
+const app = require('./app')
+const PORT = process.env.PORT || 5000
 
-const app = express()
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Working' })
-})
-
-app.listen(5000, () => console.log('> Server started'))
+app.listen(PORT, () => console.log('> Server started on port:', PORT))
