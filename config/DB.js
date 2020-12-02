abaa05ef6c8d0f79e0bf18d9074c9b778880a272
Sergey Komarov <anaxronik@gmail.com>
2020-12-02 17:11:39 +0500

    create cloud db

--- config/DB.js
+++ config/DB.js
@@ -1,2 +1,4 @@
+// module.exports.MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mern-crm'
 module.exports.MONGO_URI =
-  process.env.MONGO_URI || 'mongodb://localhost:27017/mern-crm'
+  process.env.MONGO_URI ||
+  'mongodb+srv://admin:admin@cluster0.0clzt.mongodb.net/mern-crm?retryWrites=true&w=majority'
